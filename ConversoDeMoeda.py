5a72dd8cf7eb95727be933d110dedd53702787bb
prometheuslr <prometheus.inves@gmail.com>
2023-06-23 02:31:25 -0300

    Add projeto onde converte o real em dolar, euro ou yene

---
+++ ConversoDeMoeda.py
@@ -0,0 +1,22 @@
+
+dinheiro = float(input("Quanto dinheiro você tem na carteira? \n"))
+
+moeda = int(input("Qual moeda você que converte o real? \n "
+          "1-Dolar(USD) \n 2-Euro\n 3-Yene\n"
+                  "Digite o número corespondente a moeda: "))
+
+def case1():
+    print("Com R${} você pode compra US${}".format(dinheiro, dinheiro/4.77))
+
+def case2():
+    print("Com R${} você pode compra €{}".format(dinheiro, dinheiro / 5.22))
+
+def case3():
+    print("Com R${} você pode compra JP¥{}".format(dinheiro, dinheiro * 30))
+switch = {
+    1: case1,
+    2: case2,
+    3: case3
+}
+
+switch[moeda]()

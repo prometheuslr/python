3568bcf29f986f01a1ab4b0bcfa7cb3458442fe9
prometheuslr <prometheus.inves@gmail.com>
2023-06-28 00:21:18 -0300

    Criação de um pagina em html

--- DJango/projeto de estudo alura/alura-space/setup/settings.py
+++ DJango/projeto de estudo alura/alura-space/setup/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galeria',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

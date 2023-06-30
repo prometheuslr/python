0196752e048a19f11e402e66d849d93af864847c
prometheuslr <prometheus.inves@gmail.com>
2023-06-29 22:42:46 -0300

    Finalizadoo projeto de django

--- DJango/projeto de estudo alura/alura-space/setup/settings.py
+++ DJango/projeto de estudo alura/alura-space/setup/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
 
 ]
 
@@ -118,6 +118,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+MEDIA_URL = ("/media/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
